3acda0e042d5f0aa4875c28a2f00259b816222da
cascader13 <94845349+cascader13@users.noreply.github.com>
2025-09-10 14:31:25 +0300

    3 задание

--- 2_pr/vehicles/app/TestCar.java
+++ 2_pr/vehicles/app/TestCar.java
@@ -1,4 +1,16 @@
 package vehicles.app;
 
+import vehicles.Car;
+import vehicles.ElectricCar;
+import vehicles.Vehicle;
+
 public class TestCar {
+    public static void main(String[] args) {
+        Vehicle Car1 = new Car("BMV", "112", "red", 2015, "Lari", "3450", "Gazoline");
+        Vehicle Car2 = new ElectricCar("Tesla", "204", "blue", 2020, "Ilon", "55423", 324);
+        ((Car)Car1).ToString();
+        ((ElectricCar)Car2).ToString();
+        System.out.println(((Car)Car1).vehicleType());
+        System.out.println(((ElectricCar)Car2).vehicleType());
+    }
 }
